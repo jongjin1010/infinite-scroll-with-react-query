5e0beb02d399cf1bc0d0041250225cda44c13f09
jongjin1010 <jongjin024@gmail.com>
2023-05-08 10:43:58 +0900

    add: get data from github api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useInfiniteQuery } from "react-query";
+
+interface CommentType {
+  id: number;
+  name: string;
+  description: string;
+}
 
 function App() {
+  const LIMIT = 10;
+
+  const fetchRepositories = async (page: number) => {
+    const response = await fetch(
+      `https://api.github.com/search/repositories?q=topic:react&per_page=${LIMIT}&page=${page}`
+    );
+    return response.json();
+  };
+
+  const { data, isSuccess, hasNextPage, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery(
+      "repos",
+      ({ pageParam = 1 }) => fetchRepositories(pageParam),
+      {
+        getNextPageParam: (lastPage, allPages) => {
+          const nextPage = allPages.length + 1;
+          return nextPage;
+        },
+      }
+    );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {isSuccess &&
+        data?.pages?.map((page) =>
+          page?.items?.map((comment: CommentType) => (
+            <div className="result" key={comment.id}>
+              <span>{comment.name}</span>
+              <p>{comment.description}</p>
+            </div>
+          ))
+        )}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+/**
+ * QueryClientProvider를 최상단에서 감싸주고 QueryClient instance를 client props로 넣어 애플리케이션에 연결시킨다.
+ */
+
+const queryClient = new QueryClient();
+
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
